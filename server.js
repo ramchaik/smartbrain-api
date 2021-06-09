dc007df003f581f1ebbfc31c4b4db49aefaf335e
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-06-09 23:26:16 +0530

    feat: add signin & register routes

--- server.js
+++ server.js
@@ -4,15 +4,57 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const database = {
+  users: [
+    {
+      id: "1",
+      name: "John",
+      email: "john@example.com",
+      password: "john",
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: "2",
+      name: "Sally",
+      email: "sally@example.com",
+      password: "sally",
+      entries: 0,
+      joined: new Date(),
+    },
+  ],
+};
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello world!</h1>");
+  res.json(database.users);
 });
 
 app.post("/signin", (req, res) => {
-  res.send("signin");
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    return res.json("success");
+  }
+  res.status(400).json("error logging in");
+});
+
+app.post("/register", (req, res) => {
+  const { name, email, password } = req.body;
+
+  database.users.push({
+    id: Date.now(),
+    name,
+    email,
+    password,
+    entries: 0,
+    joined: new Date(),
+  });
+
+  res.json(database.users[database.users.length - 1]);
 });
 
 app.listen(PORT, () => {
